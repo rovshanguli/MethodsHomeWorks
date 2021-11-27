624c52193f91ea2d6ec55c8af0a805f9b8226401
rovshanguli <rovshanshg@code.edu.az>
2021-11-27 19:10:31 +0400

    SumOffOddNumbers

---
+++ SumOddNumbers/SumOddNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumOddNumbers(1, 13);
+        }
+        static void SumOddNumbers(int n , int m)
+        {
+            int total = 0;
+            for (int i = n; i < m; i++)
+            {
+                
+                if (i % 2 == 1)
+                {
+                    total += i;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
