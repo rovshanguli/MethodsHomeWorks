77d663d99783a372e7a871cc9dbb4814074beef3
rovshanguli <rovshanshg@code.edu.az>
2021-11-27 19:12:28 +0400

    SelectedSimpleNumbers

---
+++ SimpleNumbers/SimpleNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SimpleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleNumbers(121);
+        }
+
+        static void SimpleNumbers(int n)
+        {
+            if(n == 0 || n == 1)
+            {
+                Console.WriteLine("Please Enter Correct Number");
+            }
+            else
+            {
+                var m = 0;
+                for(int i = 2; i < n ; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        m = m + 1;
+                    }
+                }
+                if (m > 0)
+                {
+                    Console.WriteLine("N sade eded deyil");
+                }
+                else
+                {
+                    Console.WriteLine("N sade ededdir");
+                }
+                 
+            }
+            
+        }
+    }
+}
