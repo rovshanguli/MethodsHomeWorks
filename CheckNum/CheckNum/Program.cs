663366758023c623584b5d4af74113930c36342e
rovshanguli <rovshanshg@code.edu.az>
2021-11-27 19:04:32 +0400

    SelectedNumDiviede3and7

---
+++ CheckNum/CheckNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodsPracties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check(67);
+        }
+
+        static void Check(int n)
+        {
+            if (n % 3 == 0 && n % 7 == 0)
+            {
+                Console.WriteLine("N 3 ve 7 -ye bolunur ");
+            }
+            else
+            {
+                Console.WriteLine("N 3 ve ya 7 -ye bolunmur ");
+            }
+        }
+    }
+}
