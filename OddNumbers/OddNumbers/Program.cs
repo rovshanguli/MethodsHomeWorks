74d67d528591533090e00c26642dfd9e0ae502e8
rovshanguli <rovshanshg@code.edu.az>
2021-11-27 19:07:28 +0400

    QuantityOffOddNumbers

---
+++ OddNumbers/OddNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OddNumbers(5, 2222222);
+        }
+
+        static void OddNumbers(int n , int m)
+        {
+            int count = 0;
+            for (int i  = n; i < m ; i++ )
+            {
+                if (i % 2 == 1)
+                {
+                    
+                    count++;
+                    
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
